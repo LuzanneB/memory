ffcd9b057c326ac70f91b855f643c8e2d4260cdb
Luzanne Burt <luzannea@gmail.com>
2019-07-31 09:38:39 -0400

    Updates

---
+++ precache-manifest.574010c040203e96f1725471546642dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d09163773cb34a91ec51afe93e99848",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "ce9a365c8e72e3908f85",
+    "url": "/memory/static/css/main.038d33c0.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ce9a365c8e72e3908f85",
+    "url": "/memory/static/js/main.e8688e68.chunk.js"
+  },
+  {
+    "revision": "174810366761c26401f3",
+    "url": "/memory/static/js/runtime~main.031dcd1e.js"
+  }
+]);
